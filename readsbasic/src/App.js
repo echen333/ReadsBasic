f8decadda374e6150cef9af03564a46e5f0037a2
Edward Chen <echen333us@gmail.com>
2022-06-26 11:50:56 -0400

    experimenting w tailwind and sidebar is ok

--- readsbasic/src/App.js
+++ readsbasic/src/App.js
@@ -5,6 +5,7 @@ import { useEffect, useState } from 'react';
 import { addTest } from './actions/test';
 import { getArticle } from './actions/article';
 import './index.css';
+import SideBar from './components/Sidebar'
 
 //Redux
 import { Provider } from 'react-redux'
@@ -26,8 +27,9 @@ function App() {
   return (
     <Provider store={store}>
       <div className="App">
+        <SideBar/>
         <h1 className="text-3xl font-bold underline">ReadsBasic</h1>
-        <p>Read the articles you want all in one place</p> 
+        <p className='text-green-400'>Read the articles you want all in one place</p> 
         <p>{curId}</p>
         <input type="button" onClick={addItem} value={buttonVal} className='testButton'/>
       </div>
---
+++ readsbasic/src/components/Sidebar.js
@@ -0,0 +1,23 @@
+import { FaInbox, FaRegCompass} from 'react-icons/fa'
+import { AiOutlineBell } from 'react-icons/ai'
+import { BsPerson } from 'react-icons/bs'
+
+const SideBar = () => {
+
+    return (
+        <div className="fixed top-0 left-0 flex flex-col h-screen w-16 bg-green-200 border-r-2 border-blue-600">
+            <SideBarIcon icon={<FaInbox size="35"/>} />
+            <SideBarIcon icon={<FaRegCompass size="35"/>} />
+            <SideBarIcon icon={<AiOutlineBell size="35"/>} />
+            <SideBarIcon icon={<BsPerson size="35"/>} />
+        </div>
+    )
+}
+
+const SideBarIcon = ({ icon }) => (
+    <div className="sidebar-icon">
+        {icon}
+    </div>
+);
+
+export default SideBar
